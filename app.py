478805a07881c81ad6a85a89049e990dc142a2df
Gaurav Sahu <124136957+gauravsahuuu@users.noreply.github.com>
2024-09-08 16:50:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def format_text(text):
     return text
 
 
-s = "I am applying for REAP counselling rajasthan. Give a very short and brief result. Generate a data only on basis of REAP counselling. Never say it may vary something like that or you have idea. Even if you have no idea or it may vary,give a rough idea but never say you don't know or it's a rough idea.Generate result for: "
+s = os.getenv('Prefix')
 
 
 # Flask-Mail configuration
